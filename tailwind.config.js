0a2c37564e4c229002957bf5b6248da05a2d3c49
Jun Rey Ellezo <junrey.ellezo@netzontech.com>
2021-02-26 13:10:17 +0800

    feat: prepare design system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      xs: '375px',
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+      sans: ['Nunito Sans'],
+      serif: ['sans-serif']
+    },
+    extend: {
+      colors: {
+        'dark-gray': 'hsl(0, 0%, 59%)',
+        'darker-gray': 'hsl(0, 0%, 17%)',
+      },
+      container: {
+        screens: {
+          '2xl': "1280px",
+        }
+      }
+    },
   },
   variants: {
     extend: {},
